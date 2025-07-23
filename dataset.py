6853ab463cd584a0a56f346481b3feaf185a3dcd
Veda-WhiterApps <vedavyas@whiterapps.com>
2025-07-23 22:42:19 +0530

    Update dataset.py
    Made some changes so dataset loads faster with high accuracy rates

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import os
-from torch.utils.data import Dataset
 from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as T
 
 class SegmentationDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transform=None):
@@ -8,20 +9,20 @@ class SegmentationDataset(Dataset):
         self.mask_dir = mask_dir
         self.transform = transform
         self.images = sorted(os.listdir(image_dir))
-        self.masks = sorted(os.listdir(mask_dir))
 
     def __len__(self):
         return len(self.images)
 
-    def __getitem__(self, index):
-        img_path = os.path.join(self.image_dir, self.images[index])
-        mask_path = os.path.join(self.mask_dir, self.masks[index])
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        mask_path = os.path.join(self.mask_dir, self.images[idx])
 
         image = Image.open(img_path).convert("RGB")
-        mask = Image.open(mask_path).convert("L")
+        mask = Image.open(mask_path).convert("L")  # grayscale
 
         if self.transform:
             image = self.transform(image)
             mask = self.transform(mask)
 
         return image, mask
+
